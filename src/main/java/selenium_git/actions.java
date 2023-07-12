e501c421195b44156e4958612d0e0c600e153aa2
ashuj9 <138884875+ashuj9@users.noreply.github.com>
2023-07-12 13:01:34 +0530

    cmt

--- src/main/java/selenium_git/actions.java
+++ src/main/java/selenium_git/actions.java
@@ -12,18 +12,19 @@ public class actions {
 
 	public static void main(String[] args) {
 		WebDriver d=new ChromeDriver();
-		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		d.get("https://opensource-demo.orangehrmlive.com");
+		d.get("https://www.hyrtutorials.com/");
 		d.manage().window().maximize();
-		d.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
-		d.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
-		d.findElement(By.xpath("//button[@type='submit']")).click();
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
 		Actions actions=new Actions(d);
-		By ad=(By.xpath("//a[@class='oxd-main-menu-item']"));
-		WebElement admin=d.findElement(ad);
-		By org=By.xpath("//span[text()='Organization ']");
-		WebElement orgn=d.findElement(org);
-		actions.moveToElement(admin).click().perform();
+		actions
+		.moveToElement(d.findElement(By.xpath("(//a[text()='YouTube Courses'])")))
+		.moveToElement(d.findElement(By.xpath("//a[text()='English']")))
+		.moveToElement(d.findElement(By.xpath("(//a[text()='Core Java'])[1]")))
+		.click()
+		.perform();
+		
+		
 
 	}
 
